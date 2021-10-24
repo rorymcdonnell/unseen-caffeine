40f4f87120a2d6b90ef3e67afae3a7ff13992ca2
great_hayat <greathayat@gmail.com>
2021-10-24 15:57:15 +0500

    refactor routes code

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -3,28 +3,10 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import HomeScreen from "./screens/HomeScreen";
-import ProductScreen from "./screens/ProductScreen";
-import CartScreen from "./screens/CartScreen";
-import LoginScreen from "./screens/LoginScreen";
-import RegisterScreen from "./screens/RegisterScreen";
-import ProfileScreen from "./screens/ProfileScreen";
-import ShippingScreen from "./screens/ShippingScreen";
-import PaymentScreen from "./screens/PaymentScreen";
-import PlaceOrderScreen from "./screens/PlaceOrderScreen";
-import OrderScreen from "./screens/OrderScreen";
-import UserListScreen from "./screens/UserListScreen";
-import UserEditScreen from "./screens/UserEditScreen";
-import ProductListScreen from "./screens/ProductListScreen";
-import ProductEditScreen from "./screens/ProductEditScreen";
-import OrderListScreen from "./screens/OrderListScreen";
-import SubscriptionManagement from "./screens/SubscriptionManagement";
-import SubscriptionScreen from "./screens/SubscriptionScreen";
-import SubscriptionCheckoutScreen from "./screens/SubscriptionCheckoutScreen";
-import SubscriptionListScreen from "./screens/SubscriptionList";
-import CoffeeProducts from "./screens/CoffeeProducts";
-import MerchandiseProducts from "./screens/MerchandiseProducts";
-import SubscriptionItem from "./screens/SubscriptionItem";
+import PrivateRoute from "./components/hoc/PrivateRoute";
+import { routesList } from "./routes/routesList";
+
+const routeTypes = { public: "public", private: "private" };
 
 const App = () => {
   return (
@@ -32,55 +14,17 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
-          <Route path="/order/:id" component={OrderScreen} />
-          <Route path="/shipping" component={ShippingScreen} />
-          <Route path="/payment" component={PaymentScreen} />
-          <Route path="/placeorder" component={PlaceOrderScreen} />
-          <Route path="/login" component={LoginScreen} />
-          <Route path="/register" component={RegisterScreen} />
-
-          <Switch>
-            <Route
-              exact
-              path="/profile/my-subscriptions"
-              component={SubscriptionManagement}
-            />
-            <Route path="/profile" component={ProfileScreen} />
-          </Switch>
-          <Route path="/shop" component={CoffeeProducts} exact />
-          <Route path="/merchandise" component={MerchandiseProducts} exact />
-          <Route path="/subscriptions" component={SubscriptionScreen} exact />
-          <Route path="/subscription/:id" component={SubscriptionItem} exact />
-          <Route
-            path="/checkout"
-            component={SubscriptionCheckoutScreen}
-            exact
-          />
-          <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/cart/:id?" component={CartScreen} />
-          <Route path="/admin/userlist" component={UserListScreen} />
-          <Route path="/admin/user/:id/edit" component={UserEditScreen} />
-          <Route
-            path="/admin/productlist"
-            component={ProductListScreen}
-            exact
-          />
-          <Route
-            path="/admin/productlist/:pageNumber"
-            component={ProductListScreen}
-            exact
-          />
-          <Route path="/admin/product/:id/edit" component={ProductEditScreen} />
-          <Route path="/admin/orderlist" component={OrderListScreen} />
-          <Route path="/subscriptionlist" component={SubscriptionListScreen} />
-          <Route path="/search/:keyword" component={HomeScreen} exact />
-          <Route path="/page/:pageNumber" component={HomeScreen} exact />
-          <Route
-            path="/search/:keyword/page/:pageNumber"
-            component={HomeScreen}
-            exact
-          />
-          <Route path="/" component={HomeScreen} exact />
+          {routesList?.map((route, index) =>
+            route?.routeType === routeTypes.public ? (
+              <Switch>
+                <Route exact path={route.path} component={route.component} />
+              </Switch>
+            ) : (
+              <Switch>
+                <PrivateRoute path={route.path} component={route.component} />
+              </Switch>
+            )
+          )}
         </Container>
       </main>
       <Footer />
